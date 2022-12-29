6777d823d46bdde21051b5a4eadebe4b116bac2c
dowerkali <susiloroberto@yahoo.com>
2022-12-29 13:01:46 +0700

    Inisialisasi project

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme)),
+    );
+  }
+}
---
+++ lib/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+Color primaryColor = Color(0xffFD9340);
+Color greyLight = Color(0xffE5E4E3);
+Color greyDark = Color(0xffB3B1B0);
+Color black = Color(0xff1A1919);
+Color blue = Color(0xff337EFE);
+
+TextStyle lightText = TextStyle(fontWeight: FontWeight.w300, color: black);
+TextStyle regularText = TextStyle(fontWeight: FontWeight.w400, color: black);
+TextStyle mediumText = TextStyle(fontWeight: FontWeight.w500, color: black);
+TextStyle semiboldText = TextStyle(fontWeight: FontWeight.w600, color: black);
+TextStyle boldText = TextStyle(fontWeight: FontWeight.w700, color: black);
